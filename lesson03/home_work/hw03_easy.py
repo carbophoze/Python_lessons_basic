9913e0c443075ff0b7cd5c5a7b5a453d68449ebe
Pavel Shkret <carbophoze@gmail.com>
2018-12-08 23:15:06 +0700

    Lesson03 HomeWork

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,4 @@
+__author__ = 'Шкрет Павел Александрович'
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,13 +6,30 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    p2len = len(str(int(str(number).split('.')[1])))  # Длина дробной части
+    k = p2len  # Коэффициент преобразования
+    x = number * (10 ** k)
+    while k > ndigits:
+        if x % 10 > 4:
+            x += 10
+        x //= 10
+        k -= 1
+    x = x / (10 ** ndigits)
+    if ndigits == 0:
+        return int(x)
+    return x
 
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
+print(my_round(0.0, 3))
 
+# 2.12346
+# 2.2
+# 3.0
+# 0.0
+#
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -19,10 +37,21 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
+
 def lucky_ticket(ticket_number):
+    t = [int(x) for x in str(ticket_number)]
+    if not(len(t) == 6):
+        return 'Invalid number'
+    if sum(t[:3]) == sum(t[3:]):
+        return 'Lucky ticket!'
+    return 'Nolucky ticket!'
     pass
 
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+# Lucky ticket!
+# Invalid number
+# Lucky ticket!
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,10 +1,43 @@
+__author__ = 'Шкрет Павел Александрович'
+import math
+
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+
 def fibonacci(n, m):
+    if (n < 1) or (n >= m):
+        print('Error')
+        return
+    fl = [0, 1]
+    counter = 1
+    while counter < n:
+        fl[1] = fl[0] + fl[1]
+        fl[0] = fl[1] - fl[0]
+        counter += 1
+    while counter <= m:
+        fl[1] = fl[0] + fl[1]
+        fl[0] = fl[1] - fl[0]
+        counter += 1
+        print(fl[0])
+
     pass
 
+
+fibonacci(1, 10)
+''''''
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+''''''
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
@@ -12,16 +45,76 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
+    right = len(origin_list)
+    left = 0
+    while left < right:
+        minx = left
+        for x in range(left, right):
+            if origin_list[x] < origin_list[minx]:
+                minx = x
+        t = origin_list[left]
+        origin_list[left] = origin_list[minx]
+        origin_list[minx] = t
+        left += 1
+    print(origin_list)
     pass
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
+sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+#           [-12, -11, 0, 2, 2.5, 4, 4, 10, 20]
+print('\n')
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
+def my_filter(func, lst):
+    for item in lst:
+        if func(item):
+            yield item
+    pass
+
+
+print(list(my_filter(lambda x: x > 0, [1, -2, 3, -1, 2, -3])))
+print(list(filter(lambda x: x > 0, [1, -2, 3, -1, 2, -3])))
+
+# [1, 3, 2]
+# [1, 3, 2]
+print('\n')
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+
+def line_length(a, b): # Длина вектора
+
+    return math.sqrt((b[0] - a[0])**2 + (a[1] - b[1])**2)
+
+
+def is_plgrm(_points):
+    # Перебираем если вершины указаны не по порядку
+    if line_length(_points[0], _points[1]) == line_length(_points[2], _points[3]) and \
+            line_length(_points[0], _points[1]) > 0:
+        return True
+
+    if line_length(_points[0], _points[2]) == line_length(_points[1], _points[3]) and \
+            line_length(_points[0], _points[2]) > 0:
+        return True
+
+    if line_length(_points[0], _points[3]) == line_length(_points[1], _points[2]) and \
+            line_length(_points[0], _points[3]) > 0:
+        return True
+    return False
+    pass
+
+
+a1 = [0, 0]
+a2 = [5, 1]
+a3 = [4, 2]
+a4 = [-1, 1]
+points = [a1, a2, a3, a4]
+print(is_plgrm(points))
+
+
+
 
